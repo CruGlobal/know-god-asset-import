2a960407ae1702e3c6b9dadcfb03ce9645e0ced0
Adam Meyer <adam.meyer@cru.org>
2018-08-28 13:19:20 -0400

    add timeout to allow for the translation data to be committed to API after publish

--- index.js
+++ index.js
@@ -21,89 +21,92 @@ exports.handler = (event, context, callback) => {
   let versionId = zipFilename.substring(0, zipFilename.length - 4);
   console.log('Version id: ' + versionId);
 
-  //get translations index
-  request(mobileContentAPI + '/translations?include=resource,language', (error, response, body) => {
-    const jsonResponse = JSON.parse(body);
+  //wait for translation to be saved to API
+  setTimeout(() => {
+    //get translations index
+    request(mobileContentAPI + '/translations?include=resource,language', (error, response, body) => {
+      const jsonResponse = JSON.parse(body);
 
-    //find language and resource id
-    language = jsonResponse.included.find(record => {
-      return record.type === 'language' && record.attributes.code === language;
-    });
-    resource = jsonResponse.included.find(record => {
-      return record.type === 'resource' && record.attributes.abbreviation === resource;
-    });
+      //find language and resource id
+      language = jsonResponse.included.find(record => {
+        return record.type === 'language' && record.attributes.code === language;
+      });
+      resource = jsonResponse.included.find(record => {
+        return record.type === 'resource' && record.attributes.abbreviation === resource;
+      });
 
-    //find translations id
-    let translation = jsonResponse.data.find(translation => {
-      return translation.relationships.resource.data.id === resource.id &&
-          translation.relationships.language.data.id === language.id &&
-          translation.attributes.version === Number(versionId);
-    });
+      //find translations id
+      let translation = jsonResponse.data.find(translation => {
+        return translation.relationships.resource.data.id === resource.id &&
+            translation.relationships.language.data.id === language.id &&
+            translation.attributes.version === Number(versionId);
+      });
 
-    if(!translation){
-      console.log('Translation not found.');
-      callback();
-      return;
-    }
+      if(!translation){
+        console.log('Translation not found.');
+        callback();
+        return;
+      }
 
-    //download translation
-    let tmpZip = tmp.fileSync();
-    request(mobileContentAPI + '/translations/' + translation.id)
-      .pipe(fs.createWriteStream(tmpZip.name))
-      .on('error', (err) => {
-        console.log(translation.id + ' zip download error', err);
-      })
-      .on('close', () => {
-        console.log(translation.id + ' zip file download complete.');
+      //download translation
+      let tmpZip = tmp.fileSync();
+      request(mobileContentAPI + '/translations/' + translation.id)
+          .pipe(fs.createWriteStream(tmpZip.name))
+          .on('error', (err) => {
+            console.log(translation.id + ' zip download error', err);
+          })
+          .on('close', () => {
+            console.log(translation.id + ' zip file download complete.');
 
-        let tmpExtractDir = tmp.dirSync({unsafeCleanup: true});
-        extract(tmpZip.name, {dir: tmpExtractDir.name}, (err) => {
-          if(err){
-            console.log(translation.id, err);
-            callback();
-            return;
-          }
-
-          fs.readdir(tmpExtractDir.name, (err, files) => {
-            console.log('Zip file extracted.');
-            if (!files || files.length === 0) {
-              console.log(`provided folder '${tmpExtractDir.name}' is empty or does not exist.`);
-              console.log('Make sure your project was compiled!');
-              callback();
-              return;
-            }
-
-            //only xml
-            files = files.filter(file => file.includes('.xml'));
-
-            let uploadPromises = [];
-            for (const fileName of files) {
-              // get the full path of the file
-              const filePath = path.join(tmpExtractDir.name, fileName);
-
-              // ignore if directory
-              if (fs.lstatSync(filePath).isDirectory()) {
-                continue;
+            let tmpExtractDir = tmp.dirSync({unsafeCleanup: true});
+            extract(tmpZip.name, {dir: tmpExtractDir.name}, (err) => {
+              if(err){
+                console.log(translation.id, err);
+                callback();
+                return;
               }
 
-              const fileContent = fs.readFileSync(filePath);
+              fs.readdir(tmpExtractDir.name, (err, files) => {
+                console.log('Zip file extracted.');
+                if (!files || files.length === 0) {
+                  console.log(`provided folder '${tmpExtractDir.name}' is empty or does not exist.`);
+                  console.log('Make sure your project was compiled!');
+                  callback();
+                  return;
+                }
 
-              // upload file to S3
-              uploadPromises.push(s3.putObject({
-                Bucket: s3bucket,
-                Key: path.join(translation.id, fileName),
-                Body: fileContent,
-                ACL: 'public-read'
-              }).promise());
-            }
+                //only xml
+                files = files.filter(file => file.includes('.xml'));
 
-            Promise.all(uploadPromises).then(() => {
-              console.log(translation.id + ' Complete.');
-              callback();
+                let uploadPromises = [];
+                for (const fileName of files) {
+                  // get the full path of the file
+                  const filePath = path.join(tmpExtractDir.name, fileName);
+
+                  // ignore if directory
+                  if (fs.lstatSync(filePath).isDirectory()) {
+                    continue;
+                  }
+
+                  const fileContent = fs.readFileSync(filePath);
+
+                  // upload file to S3
+                  uploadPromises.push(s3.putObject({
+                    Bucket: s3bucket,
+                    Key: path.join(translation.id, fileName),
+                    Body: fileContent,
+                    ACL: 'public-read'
+                  }).promise());
+                }
+
+                Promise.all(uploadPromises).then(() => {
+                  console.log(translation.id + ' Complete.');
+                  callback();
+                });
+
+              });
             });
-
           });
-        });
-      });
-  });
+    });
+  }, 1500);
 };
